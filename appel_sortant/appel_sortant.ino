df65e742b3bc2029eff925955aff2d330e8d2588
davkalgora <davkalgora@gmail.com>
2015-09-28 04:32:22 +0000

    modifier le 27/09/2015

---
+++ appel_sortant/appel_sortant.ino
@@ -0,0 +1,43 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(7, 8);
+// relayer la réponse de fromSerial (type SoftwareSerial) vers
+//le port serie d'Arduino (19200baud). Facile le deboggage
+void relayResponse( SoftwareSerial &fromSerial ){
+// Attention, il faut attendre que l'UART réponde
+// avant de lire mySerial (temps conseillé= 1sec)
+//
+while(fromSerial.available())
+{
+// Envoi le résultat sur la connexion série
+char c = fromSerial.read();
+Serial.print(c);
+}
+}
+void setup(){
+  mySerial.begin(19200);
+  // GPRS baud rate
+  Serial.begin(19200);
+  // GPRS baud rate
+  delay(2000);
+  mySerial.println("ATDxxxxxxxxx;"); // xxxxxxxxx est le numéro a appeler.
+  // NB: Une boucle while serait plus appropriée que la section de code ci-dessous.
+  //
+  //Je vous propose d'appeler la fonction suivante:
+  //relayResponse( mySerial )
+  if(mySerial.available()){
+    char c = mySerial.read();
+    Serial.print( c );
+  }
+  delay(10000);
+  delay(10000);
+  mySerial.println("ATH"); // Fin d'appel.
+  if(mySerial.available()) // Voir commentaire précédent.
+  {
+    char c = mySerial.read();
+    Serial.print( c );
+  }
+}
+void loop(){
+  // Ne rien faire
+}
+
